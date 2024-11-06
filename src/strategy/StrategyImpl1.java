7db0d036aa287d06318724d1ccf3b5e532e0c938
JMOROKO <j_moroko@etu.enset-media.ac.ma>
2024-11-06 16:15:37 +0000

    design pattern strategy

---
+++ src/strategy/StrategyImpl1.java
@@ -0,0 +1,8 @@
+public class StrategyImpl1 implements Strategy {
+    @Override
+    public void apply() {
+        System.out.println("................................");
+        System.out.println("First strategy");
+        System.out.println("................................");
+    }
+}
---
+++ src/strategy/StrategyImpl2.java
@@ -0,0 +1,10 @@
+package strategy;
+
+public class StrategyImpl1 implements Strategy {
+    @Override
+    public void apply() {
+        System.out.println("................................");
+        System.out.println("First strategy");
+        System.out.println("................................");
+    }
+}
